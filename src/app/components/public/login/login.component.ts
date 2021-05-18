199dcad2fcd0ce3a4a7d3540a5142ef62a745d1d
JasperGriffin <33640253+JasperGriffin@users.noreply.github.com>
2021-05-18 20:08:37 +0100

    Began implementation of the login page

--- src/app/components/public/login/login.component.ts
+++ src/app/components/public/login/login.component.ts
@@ -2,6 +2,11 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { KeyloggerService } from '../../../services/keylogger.service';
+import { ParserService } from '../../../services/parser.service';
+import { AuthService } from '../../../services/auth.service';
+import { ErrorHandlerService } from '../../../services/error-handler.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,10 +14,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   
+  userInput: string = 'this is a test';
   infoMessage: string = ''; 
+  errorMessage: string = ''; 
+  valid: boolean = true; 
+  anotherValid: boolean = true;
 
   constructor (
     private router: Router,
+    private keylogger: KeyloggerService,
+    private parser: ParserService,
+    private auth: AuthService,
+    private error: ErrorHandlerService,
     private route: ActivatedRoute
   ) {}
 
@@ -30,19 +43,38 @@ export class LoginComponent implements OnInit {
     username : new FormControl('', Validators.compose([
       Validators.required,
       Validators.pattern('User#[0-9]{4}')])),
+
     sentence : new FormControl('', Validators.compose([
       Validators.required,
-      Validators.pattern('test')]))
+      Validators.pattern(this.userInput)]))
   });
 
   get username() { return this.userForm.get('username');  }
 
   get sentence() { return this.userForm.get('sentence'); }
 
+  //keyboard listeners
+  onKeyUp(event: any) { this.keylogger.onKeyUp(event); }
+
+  onKeyDown(event: any) { this.keylogger.onKeyDown(event); }
+
+  onKeyPress(event: any) { this.keylogger.onKeyPress(event); }
+
   //mouse listener
   @HostListener('mousemove', ['$event'])
   handleMousemove(event: MouseEvent) {
-    //this.keylogger.onMouseMove(event); 
+    this.keylogger.onMouseMove(event); 
+  }
+
+  validateInput(event: any) {
+    /*validateInput doesn't read ctrl, tab, etc so this needs to be done elsewhere*/
+    //this.num = event.path[this.keyboardID].id;
+
+    const regex = new RegExp(event.target.value);
+    if (!regex.test(this.userInput) ||!this.valid) {
+      this.valid = false; 
+      console.log("This sentence is wrong"); 
+    }
   }
 
   clearValue() {
@@ -63,10 +95,14 @@ export class LoginComponent implements OnInit {
   }
   
   onSubmit() {
+    
+    this.keylogger.initialiseUser(this.username?.value);
+
 
     if (this.onSubmitValidate()) {
-      const loggedIn: string[] = ['/home'];
-      this.router.navigate(loggedIn);
+      const userTemplate = this.keylogger.getUser();
+      console.log(userTemplate); 
+
     }
   
   }
